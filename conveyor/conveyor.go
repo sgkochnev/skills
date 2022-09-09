9610284662cfdcd9bde252c9731d49eb1bb2da21
Sergey Kochnev <skochnev69@gmail.com>
2022-09-09 15:06:46 +0300

    Added solution for conveyor task

--- conveyor/conveyor.go
+++ conveyor/conveyor.go
@@ -1,5 +1,53 @@
 package conveyor
 
+import (
+	"sync"
+)
+
 func RunConveyor(tasksFlow ...task) {
-	// implement conveyor in here
+
+	wg := sync.WaitGroup{}
+	var chOut chan interface{}
+	for i, t := range tasksFlow {
+
+		wg.Add(1)
+
+		switch i {
+		case 0:
+			chOut = make(chan interface{})
+			go func(t task, out chan interface{}) {
+				t(nil, out)
+				close(out)
+				wg.Done()
+			}(t, chOut)
+
+		case len(tasksFlow) - 1:
+			chIn := f(chOut)
+			go func(t task, in chan interface{}) {
+				t(in, nil)
+				wg.Done()
+			}(t, chIn)
+
+		default:
+			chIn := f(chOut)
+			chOut = make(chan interface{})
+			go func(t task, in, out chan interface{}) {
+				t(in, out)
+				close(out)
+				wg.Done()
+			}(t, chIn, chOut)
+		}
+	}
+	wg.Wait()
+}
+
+func f(chIn chan interface{}) chan interface{} {
+	ch := make(chan interface{})
+	go func() {
+		for v := range chIn {
+			ch <- v
+		}
+		close(ch)
+	}()
+	return ch
 }
