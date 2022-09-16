515b1359057114cec8a78a4b26cf3a91183d62a9
sgkochnev <50366716+sgkochnev@users.noreply.github.com>
2022-09-16 22:55:19 +0300

    Fix conveyor

--- conveyor/conveyor.go
+++ conveyor/conveyor.go
@@ -6,48 +6,22 @@ import (
 
 func RunConveyor(tasksFlow ...task) {
 
-	wg := sync.WaitGroup{}
-	var chOut chan interface{}
-	for i, t := range tasksFlow {
+	wg := &sync.WaitGroup{}
+	var out chan interface{}
 
+	for _, t := range tasksFlow {
 		wg.Add(1)
 
-		switch i {
-		case 0:
-			chOut = make(chan interface{})
-			go func(t task, out chan interface{}) {
-				t(nil, out)
-				close(out)
-				wg.Done()
-			}(t, chOut)
+		in := out
+		out = make(chan interface{})
 
-		case len(tasksFlow) - 1:
-			chIn := fanIn(chOut)
-			go func(t task, in chan interface{}) {
-				t(in, nil)
-				wg.Done()
-			}(t, chIn)
+		go func(t task, in, out chan interface{}) {
+			defer close(out)
+			defer wg.Done()
+			t(in, out)
 
-		default:
-			chIn := fanIn(chOut)
-			chOut = make(chan interface{})
-			go func(t task, in, out chan interface{}) {
-				t(in, out)
-				close(out)
-				wg.Done()
-			}(t, chIn, chOut)
-		}
+		}(t, in, out)
 	}
-	wg.Wait()
-}
 
-func fanIn(chIn chan interface{}) chan interface{} {
-	ch := make(chan interface{})
-	go func() {
-		for v := range chIn {
-			ch <- v
-		}
-		close(ch)
-	}()
-	return ch
+	wg.Wait()
 }
